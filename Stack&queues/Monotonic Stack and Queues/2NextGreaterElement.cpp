495edafcaa3d47b4136330d357296748621caa0a
ashuk-11 <ashutosh110805@gmail.com>
2025-12-16 23:32:43 +0530

    Monotonic Stack(NGE)

--- Stack&queues/Monotonic Stack and Queues/2NextGreaterElement.cpp
+++ Stack&queues/Monotonic Stack and Queues/2NextGreaterElement.cpp
@@ -32,3 +32,45 @@ public:
         return ans;
     }
 };
+
+//------------------------------- 2nd Approach -------------------------------
+
+// Using monotonic stack
+// top of stack is next greater element of present element
+// if present is greater than top, then keep popping till either stack becomecomes empty, or top os stack lis lesser than present element
+// if popping causes stack empty, then no nge and put -1
+// else, and we get a greater element at top , then nge of present element is top of stack
+// then finally put the present element in stack 
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        //unordered map for storing next greater for each el in nums2
+        unordered_map<int, int> nge;
+
+        // Monotonic stack which is storing elements in decreasing order only
+        stack<int> st;
+
+        for(int i= nums2.size()-1; i>= 0; i--){
+
+            // keep popping till top is lesser than preset el
+            while(!st.empty() && st.top()<= nums2[i]){
+                st.pop();
+            }
+            //after popping if empty
+            if(st.empty()) nge[nums2[i]]= -1;
+            //not empty, then top of stack is nge of present el
+            else nge[nums2[i]]= st.top();
+
+            //lastly, put the present el in stack
+            st.push(nums2[i]);
+        }
+
+        vector<int> ans;
+        for(auto el: nums1){
+            // nge stores next greaster of each, so get it for each el in nums1
+            ans.push_back(nge[el]);
+        }
+        return ans;
+    }
+};
